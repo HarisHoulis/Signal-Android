3c631e439f540b56e23bb838fb5dbec724b14357
Pablo Cambre <pablo.cambre@warnermedia.com>
2023-08-29 14:22:07 +0200

    Fix AnyPhoto

--- feature/discover/src/main/java/com/app/signal/discover/root/model/AnyPhoto.kt
+++ feature/discover/src/main/java/com/app/signal/discover/root/model/AnyPhoto.kt
@@ -3,4 +3,9 @@ package com.app.signal.discover.root.model
 import com.app.signal.domain.model.photo.Image
 import com.app.signal.domain.model.photo.Photo
 
-data class AnyPhoto(override val id: String, override val img: Image, override val title: String) : Photo
+data class AnyPhoto(
+    override val id: String,
+    override val img: Image,
+    override val title: String,
+    override val isFavourite: Boolean
+) : Photo
