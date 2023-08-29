f96a5ed97e5cefccfcb11f472c5f35bcea24fe43
Pablo Cambre <pablo.cambre@warnermedia.com>
2023-08-29 14:55:57 +0200

    Fix DiscoveryVM

--- data/src/test/java/com/app/signal/data/service/PhotoServiceImplTest.kt
+++ data/src/test/java/com/app/signal/data/service/PhotoServiceImplTest.kt
@@ -3,7 +3,6 @@ package com.app.signal.data.service
 import app.cash.turbine.test
 import com.app.signal.data.service.photo.PhotoServiceImpl
 import com.app.signal.domain.model.State
-import com.app.signal.domain.repository.IntState
 import com.app.signal.domain.repository.PhotoRepository
 import com.app.signal.domain.service.AppStorage
 import io.mockk.every
@@ -12,8 +11,6 @@ import io.mockk.verify
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.test.runTest
-import org.junit.jupiter.api.AfterEach
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
 @OptIn(ExperimentalCoroutinesApi::class)
